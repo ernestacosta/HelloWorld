71f927c08bfe4e316e3c84960ffbc40505352486
Ernest Acosta <ernestacosta@icloud.com>
2022-06-16 11:15:10 -0700

    initial commit and changed code

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please enter your name:");
+var customerName = Console.ReadLine();
+Console.WriteLine($"Hello, {customerName}!");
